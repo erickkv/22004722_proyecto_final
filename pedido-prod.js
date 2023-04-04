8b67849294ee6412b86f1b88137a6d3ef564f3bc
elquees <erickkiehnle@gmail.com>
2023-04-03 21:42:02 -0600

    avances pedidos

--- pedido-prod.js
+++ pedido-prod.js
@@ -6,4 +6,11 @@ let cant = document.getElementById("cant-pedido");
 window.comunicacion.enviarInfoProds(function(event, args) {
     id.value = args[0][0];
     nombre.value = args[0][1];
+    let proveedores = args[1];
+    proveedores.forEach(element => {
+        let opcion = document.createElement('option')
+        opcion.setAttribute('value',element['nombre_proveedor']);
+        opcion.text = element['nombre_proveedor'];
+        proveedor.add(opcion);
+    });
 })
